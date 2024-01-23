588a5643c61c8157121bed64091db392c6876d7a
flare67 <viktorkrastev1@gmail.com>
2024-01-23 08:52:14 +0200

    two more excercises

---
+++ evenNumberSum.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        int sum = 0;
+        int sumTwo = 0;
+        for (int i = 0; i <= 100; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine(sum);
+
+        for (int i = 0;i <= 50; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sumTwo += i;
+            }
+        }
+        Console.WriteLine($"The sum of the even numbers between 1 and 50 is: {sumTwo} ");
+
+    }
+}
---
+++ randNumberGuesser.cs
@@ -0,0 +1,36 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    public static void Main()
+    {
+        int randNumb = new Random().Next(1, 100);
+        int typNum;
+        string input;
+
+        Console.WriteLine("Guess the number between 1 and 100: ");
+        input = Console.ReadLine();
+        Int32.TryParse(input, out typNum);
+
+        while (typNum != randNumb)
+        {
+            if (randNumb > typNum)
+            {
+                Console.WriteLine("The number is higher,try again: ");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out typNum);
+            }
+            else if (randNumb < typNum)
+            {
+                Console.WriteLine("The number is lower,try again: ");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out typNum);
+            }
+            else 
+            {
+                Console.WriteLine("Gongrats you guessed it!!!");
+            }
+        }
+        Console.WriteLine($"The number was{randNumb}");
+    }
+}
